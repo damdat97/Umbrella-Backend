ded821de3958d6244880d8b32b23e178891b452e
TangThiHoa <hoarua592001@gmail.com>
2022-08-01 15:43:13 +0700

    1/8 - Hoa thêm repository , service , controller của image

---
+++ src/main/java/beumbrella/controller/ImageController.java
@@ -0,0 +1,28 @@
+package beumbrella.controller;
+
+import beumbrella.model.Image;
+import beumbrella.service.impl.ImgServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@Controller
+@RequestMapping("/images")
+public class ImageController {
+    @Autowired
+    ImgServiceImpl imageService;
+
+    @GetMapping()
+    public ResponseEntity<Iterable<Image>> findAllImg(){
+        return new ResponseEntity<>(imageService.findAllImg(),HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Image> save(@RequestBody Image image) {
+        imageService.save(image);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}

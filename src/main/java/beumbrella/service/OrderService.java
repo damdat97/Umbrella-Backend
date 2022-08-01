ff2b254d76c92248efa57fa49fd8e641bb3e6cbd
damdat97 <dam.dat97@gmail.com>
2022-08-01 15:13:36 +0700

    fix Service

--- src/main/java/beumbrella/service/OrderService.java
+++ src/main/java/beumbrella/service/OrderService.java
@@ -2,6 +2,6 @@ package beumbrella.service;
 
 import beumbrella.model.Order;
 
-public interface OrderService extends IGeneralService<Order> {
+public interface OrderService extends GeneralService<Order> {
     Iterable<Order> findByUserId(Long id);
 }
